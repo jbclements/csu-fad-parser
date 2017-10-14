c874135de44b40eaab1bd9f061f6f03c40957661
John Clements <clements@racket-lang.org>
2017-10-14 16:48:32 -0700

    add deps

--- info.rkt
+++ info.rkt
@@ -8,7 +8,8 @@
                "parser-tools-lib"
                "plot-gui-lib"
                "plot-lib"
-               "typed-racket-lib"))
+               "typed-racket-lib"
+               "csse-scheduling"))
 (define build-deps '("rackunit-lib"
                      "wxme-lib"
                      "typed-racket-more"))
