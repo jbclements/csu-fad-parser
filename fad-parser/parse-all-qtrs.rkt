4058ddeb7fd7d000c4fd3e68450824e1d8a0ad5b
John Clements <clements@racket-lang.org>
2017-12-01 16:29:45 -0800

    new file to parse all quarters, write out as rktd files

---
+++ fad-parser/parse-all-qtrs.rkt
@@ -0,0 +1,14 @@
+#lang typed/racket
+
+;; parse all of the fad reports, write them to corresponding
+;; names in the same directory.
+
+(require "one-quarter-data.rkt"
+         "parsed-data-defn.rkt")
+
+(parameterize ([pretty-print-columns 500])
+  (for ([qtr (in-list (qtrs-available))])
+    (call-with-output-file (~a "/tmp/fad-"qtr"-parsed.rktd")
+      #:exists 'truncate
+      (λ ([port : Output-Port]) (pretty-write (qtr->parsed qtr) port)))))
+
