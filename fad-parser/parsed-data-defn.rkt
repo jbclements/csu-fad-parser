e23811cfe5909c7ac6e9ec5dc317cd26c570f087
John Clements <clements@racket-lang.org>
2017-12-06 16:38:47 -0800

    got sequences parsing

--- fad-parser/parsed-data-defn.rkt
+++ fad-parser/parsed-data-defn.rkt
@@ -8,6 +8,7 @@
          (struct-out Parsed)
          (struct-out Offering)
          (struct-out FacultyOffering)
+         (struct-out OfferingSequence)
          (struct-out Special)
          Level
          AssocLine)
@@ -19,8 +20,7 @@
                 [depts : (Listof Dept)]
                 [offerings : (Listof Offering)]
                 [faculty-offerings : (Listof FacultyOffering)]
-                [atoms : (Listof (Listof AssocLine)
-                                 #;CourseAtom)])
+                [sequences : (Listof OfferingSequence)])
   #:prefab)
 
 ;; represents a department's name, summary info, and instructors
@@ -34,14 +34,14 @@
 
 ;; represents information about an instructor, independent
 ;; of the courses he or she is teaching.
-(define-struct Instructor ((header : AssocLine) 
+(struct Instructor ((header : AssocLine) 
                            (summary : AssocLine)
                            (specials : (Listof Special))
                            [home? : Boolean])
   #:prefab)
 
 ;; an offering is a group of students signed up for a class.
-(define-struct Offering
+(struct Offering
   ([subject : String]
    [coursenum : String]
    [section : Natural]
@@ -56,7 +56,7 @@
 (define-type Level (U "LD" "UD" "GD"))
 
 ;; an offerfac describes an instructor's involvement with a section
-(define-struct FacultyOffering
+(struct FacultyOffering
   ([subject : String]
    [coursenum : String]
    [section : Natural]
@@ -66,37 +66,27 @@
    [dwtu : Real])
   #:prefab)
 
-;; NOTE: looks like this isn't currently used.
-;; a course-atom is a tuple of prefix X number X section X sequence X instructor,
-;; and all of the info that depends on that
-#;(define-struct CourseAtom
-  ([prefix : String]
-   [course-num : String]
+;; an offerseq describes the information associated specifically with a
+;; single sequence
+(struct OfferingSequence
+  ([subject : String]
+   [coursenum : String]
    [section : Natural]
-   [sequence : Natural]
    [instructor : String]
-   [discipline : Natural]
-   [level : Level]
-   [enrollment : Natural]
-   [group-code : Natural]
-   [team-teach-frac : Nonnegative-Real]
-   [scu : Nonnegative-Real]
-   [wtu : Nonnegative-Real]
-   [contact-hours : Nonnegative-Real]
-   [time-stop : String]
-   [space : String]
+   [sequence : Natural]
    [days : String]
-   [facility : String]
-   [facility-type : String]
    [time-start : String]
-   [a-ccu : String]
+   [time-stop : String]
    [tba-hours : Nonnegative-Real]
-   [classification : Natural])
+   [facility : String]
+   [space : String]
+   [facility-type : String]
+   [team-teach-frac : Nonnegative-Real])
   #:prefab)
 
 ;; represents a special assignment (such as course planning or
 ;; development)
-(define-struct Special
+(struct Special
   ([description : String]
    [scu : Nonnegative-Real]
    [contact-hours : Nonnegative-Real]
