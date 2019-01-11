8d88d8fb956fb5da52c2b04a5d97b5699c529166
John Clements <clements@racket-lang.org>
2019-01-10 16:10:41 -0800

    refactor to use csv-writing

--- fad-parser/tsv-export.rkt
+++ fad-parser/tsv-export.rkt
@@ -3,30 +3,30 @@
 ;; export lists of values as TSV files to be imported by postgres
 (provide tsv-export)
 
+(require/typed csv-writing
+               [#:opaque CsvPrintingParams csv-printing-params?]
+               [make-csv-printing-params
+                (#:column-separator String #:table-cell->string (Any -> String) -> CsvPrintingParams)]
+               [display-table ((Listof (Listof DbVal)) Output-Port #:printing-params CsvPrintingParams -> Void)])
+
 (require/typed db
                [#:opaque SqlNull sql-null?])
 
 (define-type DbVal (U Boolean 'null String Natural SqlNull))
 
-(define-type Serializable (U String Natural))
-
 ;; given a filename and a list of records (lists),
 ;; output the records in tab-separated format to the given filename
 (define (tsv-export [filename : Path-String]
                     [records : (Listof (Listof DbVal))])
-  (with-output-to-file filename
-    (lambda ()
-      (for ([c (in-list records)])
-        (define data (sanitize-row c))
-        (display (apply ~a (append (add-between data "\t") (list "\n"))))))
+  (call-with-output-file filename
+    (lambda (port)
+      (display-table records port #:printing-params postgres-printing-params))
     #:exists 'truncate))
 
-;; given a list of values, sanitize them to be db-friendly
-(define (sanitize-row [row : (Listof DbVal)]) : (Listof Serializable)
-  (map sanitize-value row))
+
 
 ;; given a value, produce the sanitized db-friendly version
-(define (sanitize-value [d : DbVal]) : Serializable
+(define (postgres-convert-value [d : Any]) : String
   (cond [(sql-null? d) "\\N"]
         [(equal? d 'null)
          "\\N"]
@@ -42,12 +42,23 @@
                   "no bad chars allowed in strings: ~e\n"
                   d))
          d]
-        [(exact-integer? d) d]))
+        [(exact-integer? d) (number->string d)]
+        [else (error 'postgres-convert-value
+                     "unexpected postgres value: ~e"
+                     d)]))
+
+(define postgres-printing-params : CsvPrintingParams
+  (make-csv-printing-params
+   #:column-separator "\t"
+   #:table-cell->string postgres-convert-value))
 
 (module+ test
   (require typed/rackunit)
 
-  (check-equal? (sanitize-row '(34 #f null "BOOBOAH"))
+  (check-equal? (postgres-convert-value 34) "34")
+  (check-equal? (postgres-convert-value #f) "FALSE")
+  (check-equal? (postgres-convert-value 'null) "\\N")
+  #;(check-equal? (sanitize-row '(34 #f null "BOOBOAH"))
                 '(34 "\\N" "\\N" "BOOBOAH")
    )
   )
