8746a39b965f2264a1204323fa0bd5168485de94
John Clements <clements@racket-lang.org>
2024-07-05 12:19:56 -0700

    don't run by default

--- fad-parser/scripts/regression-test.rkt
+++ fad-parser/scripts/regression-test.rkt
@@ -1,10 +1,11 @@
 #lang racket
 
-(filter
- (λ (x) (not (second x)))
-(for/list ([f (in-list (directory-list "/tmp/"))]
-           #:when (regexp-match #px"fad-2...-parsed.rktd" f))
-  (list
-   f
-   (equal? (file->value (build-path "/tmp/" f))
-          (file->value (build-path "/Users/clements/clements/datasets/FAD/parsed/" f))))))
+(define (go)
+  (filter
+   (λ (x) (not (second x)))
+   (for/list ([f (in-list (directory-list "/tmp/"))]
+              #:when (regexp-match #px"fad-2...-parsed.rktd" f))
+     (list
+      f
+      (equal? (file->value (build-path "/tmp/" f))
+              (file->value (build-path "/Users/clements/clements/datasets/FAD/parsed/" f)))))))
